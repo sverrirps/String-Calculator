69dd6df057a12b3f24f755a64adc0dfb68fe685a
sverrirps <sverrirs13@ru.is>
2014-10-12 12:55:37 +0000

    Bjo til Calculator.java og CalculatorTest.java, buinn ad redda PATH i Environment variables og allt ad rulla af stad

---
+++ stringcalculator/src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest{
+
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+	}
+
+	@Test
+
+	
+}
